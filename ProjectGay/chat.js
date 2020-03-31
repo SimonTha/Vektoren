715b8414dc1e0b91ccb35cefebc5619b391e2386
SStudioGames <62803133+SStudioGames@users.noreply.github.com>
2020-03-31 14:02:14 +0200

    Update chat.js

--- ProjectGay/chat.js
+++ ProjectGay/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('vektoren.netlify.com:8886');
+var socket = io.connect('https://vektoren.netlify.com.8886/projectgay/');
 var btn = document.getElementById('send');
 btn.addEventListener('click', function(){
   socket.emit('chat', "gay");
