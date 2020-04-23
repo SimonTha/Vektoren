9b1fc24afc70aaafd98ace7ff401e7fcb588647d
SStudioGames <SStudioGames@gmail.com>
2020-04-23 14:24:21 +0200

    delete all

--- ProjectGay/chat.js
+++
@@ -1,8 +0,0 @@
-var socket = io.connect('https://vektoren.netlify.com.8886');
-var btn = document.getElementById('send');
-btn.addEventListener('click', function(){
-  socket.emit('chat', "gay");
-});
-socket.on('chat', function(data){
-  console.log(data)
-})
